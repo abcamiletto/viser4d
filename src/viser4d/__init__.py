fc82d1af290745fe65de4144be1f07008f59576e
Andrea Boscolo Camiletto <abcamiletto@gmail.com>
2026-02-15 03:59:59 -0800

    fix

--- src/viser4d/__init__.py
+++ src/viser4d/__init__.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
+from importlib.metadata import version
 import viser as _viser
 
 from .op import CompressionMode
 from .server import Viser4dServer
 
-__all__ = ["CompressionMode", "Viser4dServer"]
+__version__ = version("viser4d")
+
+__all__ = ["CompressionMode", "Viser4dServer", "__version__"]
 
 
 def __getattr__(name: str):
